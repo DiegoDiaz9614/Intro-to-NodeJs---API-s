63976f49528f47e85447931da93d474b9e4ca3b7
DiegoDiaz9614 <diegodiaz624@yahoo.com>
2025-04-23 13:31:58 -0600

    Student API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongodb = require("./db/connect");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
